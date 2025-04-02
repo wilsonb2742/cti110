be788f1b382a7d9dc71830f35d90cdcf2234ef0c
wilsonb2742 <wilsonb2742@student.faytechcc.edu>
2025-04-02 14:48:52 -0400

    Add files via upload

---
+++ P1LAB1_WilsonBryanna.py
@@ -0,0 +1,12 @@
+# Bryanna Wilson
+# 04/02/2025  # Replace this with the actual date
+# P1LAB1 - Introduction to Python 3
+# This program prompts the user to enter their first and last names separately
+# and then prints a welcome message.
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Welcome to this course,", first_name, last_name, "!")
